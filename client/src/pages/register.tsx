e4156e2f3272dd7ba6f4e61745c5aff9700de6a9
Rickson Yang <rixin.yang.2001@gmail.com>
2021-04-13 22:19:34 -0400

    push to login after registration

--- client/src/pages/register.tsx
+++ client/src/pages/register.tsx
@@ -1,42 +1,54 @@
 import { Button } from "@chakra-ui/button";
 import { Box } from "@chakra-ui/layout";
 import { Form, Formik } from "formik";
+import { useRouter } from "next/router";
 import { registerUser } from "../api/routes/users";
 import { Container } from "../components/Container";
 import { InputField } from "../components/InputField";
 import { getErrorMap } from "../utils/getErrorMap";
 
-const Register = () => (
-  <Container height="100vh">
-    <Box m="auto">
-      <Formik
-        initialValues={{ username: "", email: "", password: "" }}
-        onSubmit={(values, { setErrors }) => {
-          registerUser(values).catch((err) => {
-            setErrors(getErrorMap(err.response.data.errors));
-          });
-        }}
-      >
-        <Form>
-          <InputField name="username" placeholder="username" label="Username" />
-          <Box mt={4}>
-            <InputField name="email" placeholder="email" label="Email" />
-          </Box>
-          <Box mt={4}>
+const Register = () => {
+  const router = useRouter();
+
+  return (
+    <Container height="100vh">
+      <Box m="auto">
+        <Formik
+          initialValues={{ username: "", email: "", password: "" }}
+          onSubmit={async (values, { setErrors }) => {
+            const response = await registerUser(values).catch((err) => {
+              setErrors(getErrorMap(err.response.data.errors));
+            });
+            if (response && response.data) {
+              router.push("/login");
+            }
+          }}
+        >
+          <Form>
             <InputField
-              name="password"
-              placeholder="password"
-              label="Password"
-              type="password"
+              name="username"
+              placeholder="username"
+              label="Username"
             />
-          </Box>
-          <Button mt={4} type="submit" colorScheme="teal">
-            register
-          </Button>
-        </Form>
-      </Formik>
-    </Box>
-  </Container>
-);
+            <Box mt={4}>
+              <InputField name="email" placeholder="email" label="Email" />
+            </Box>
+            <Box mt={4}>
+              <InputField
+                name="password"
+                placeholder="password"
+                label="Password"
+                type="password"
+              />
+            </Box>
+            <Button mt={4} type="submit" colorScheme="teal">
+              register
+            </Button>
+          </Form>
+        </Formik>
+      </Box>
+    </Container>
+  );
+};
 
 export default Register;
